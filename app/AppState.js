f240767986c99f66309cdb891c83a2a51acad308
Casey Collins <collinscasey.homes@gmail.com>
2022-07-26 15:24:57 -0600

    changes

--- app/AppState.js
+++ app/AppState.js
@@ -1,1 +1,24 @@
-import { Hero } from "./Hero.js"
+import { snacks } from "./snacks.js"
+
+
+class AppState extends EventEmitter {
+  /** @type {import('./Models/Value').Value[]} */
+
+  snacks = [
+    new snacks('Mountain Sprite', 2.50),
+    new snacks('Root Dew', 5.75)
+  ]
+}
+
+export const ProxyState = new Proxy(new AppState(), {
+  get(target, prop) {
+    isValidProp(target, prop)
+    return target[prop]
+  },
+  set(target, prop, value) {
+    isValidProp(target, prop)
+    target[prop] = value
+    target.emit(prop, value)
+    return true
+  }
+})
--- app/Models/Hero.js
+++
@@ -1,6 +0,0 @@
-export class Hero {
-    constructor(name, price) {
-        this.name = name,
-            this.price = price
-    }
-}
